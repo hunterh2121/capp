7210e14fe39f9f4f496deeff042eeea5438715ce
hunterh2121 <havenshunterbiz@gmail.com>
2025-01-03 11:47:01 -0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/cleaner",
+  base: "/app/",
   plugins: [react()],
   resolve: {
     alias: {
